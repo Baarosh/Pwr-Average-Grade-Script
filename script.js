49083b8c3c926e623669872c42122cbc3fa043a5
Baarosh <norbert.lipinski96@gmail.com>
2020-03-31 19:53:39 +0200

    First version of script

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const semesters = document.querySelectorAll('.semester')
-let validSemesters = 0
+const validSemesters = []
+let gradeSum = 0
+let ectsSum = 0
 
 semesters.forEach(semester => {
-  const row = semester.querySelector('tr')
-  const grades = row.querySelectorAll('td')
-  if (!grades[5].innerText === '---') {
-    validSemesters += 1
-    semesters
+  const firstRow = semester.querySelector('tr')
+  const firstRowData = firstRow.querySelectorAll('td')
+  if (firstRowData[5].innerText !== '---' && validSemesters.length < 2) {
+    validSemesters.push(semester)
   }
 })
-const row = semester.querySelector('tr')
-console.log(row)
-const grades = row.querySelectorAll('td')
-console.log(grades)
 
-console.log(grades[5].innerText)
-console.log(grades[7].innerText)
+validSemesters.forEach(semester => {
+  const rows = semester.querySelectorAll('tr')
+  rows.forEach(row => {
+    const rowData = row.querySelectorAll('td')
+    gradeSum +=
+      parseFloat(rowData[5].innerText) * parseFloat(rowData[7].innerText)
+    ectsSum += parseFloat(rowData[7].innerText)
+  })
+})
+
+console.log(`gradeSum : ${gradeSum}`)
+console.log(`ectsSum : ${ectsSum}`)
+console.log(`Average grade : ${gradeSum / ectsSum}`)
